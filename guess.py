97f2985ac71186ce744b19b8e059c24b052ed309
Lianzhen <lianzhenw01@gmail.com>
2019-12-27 15:36:26 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,9 +1,13 @@
 import random
-r = random.randint(1, 100)
+a = input('請輸入初始數字: ')
+a = int(a)
+b = input('請輸入最後數字: ')
+b = int(b)
+r = random.randint(a, b)
 times = 0
 
 while times >= 0:
-	guess = input('請猜一個數字1~100: ')
+	guess = input('請猜一個數字: ')
 	guess = int(guess)
 	times = times + 1
 	print('你猜了', times, '次')
