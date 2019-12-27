974b981fa55aa4cd7802ec61d3bbbfbb702938e5
Lianzhen <lianzhenw01@gmail.com>
2019-12-27 15:22:34 +0800

    add times

--- guess.py
+++ guess.py
@@ -1,15 +1,16 @@
 import random
 r = random.randint(1, 100)
-times = 1
+times = 0
 
-while times >= 1:
+while times >= 0:
 	guess = input('請猜一個數字1~100: ')
 	guess = int(guess)
 	times = times + 1
+	print('你猜了', times, '次')
 	if guess < r:
 		print('比答案小')
 	elif guess > r:
 		print('比答案大')
 	else:
-		print('終於猜對了')
+		print('BINGO猜對了')
 		break
